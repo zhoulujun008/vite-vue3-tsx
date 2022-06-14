5b09c4f2c52fc5aff1320e20a336f03dee8dd1c1
andyljzhou <andyljzhou@tencent.com>
2022-06-14 10:17:07 +0800

    feat: copy for vue2

---
+++ src/pages/share-panel/index.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+    setup() {
+        return () => (
+            <div class='full-height flex-column'>
+               面板
+            </div>
+        );
+    },
+});
