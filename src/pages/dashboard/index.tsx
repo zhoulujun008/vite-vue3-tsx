af752ca90534dce0352e51a926d43eafc69f512f
andyljzhou <andyljzhou@tencent.com>
2022-06-15 17:26:55 +0800

    feat: 项目国际化……

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,15 @@
+import { defineComponent } from 'vue';
+import { Exception } from 'bkui-vue';
+export default defineComponent({
+  props: {
+    type: String,
+    msg: String,
+  },
+  render() {
+    return () => (
+            <Exception class='exception-wrap-item' type={this.type}>
+                <span>{this.$t('国际化示例')}</span>
+            </Exception>
+    );
+  },
+});
