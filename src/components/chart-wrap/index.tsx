9a23256bf00e5f810f1be5edaf5c95e88bdd0328
andyljzhou <andyljzhou@tencent.com>
2022-06-20 11:10:26 +0800

    feat: 项目架构调整……

--- src/components/chart-wrap/index.tsx
+++ src/components/chart-wrap/index.tsx
@@ -1,10 +1,19 @@
-import { defineComponent } from 'vue';
+import { defineComponent, h } from 'vue';
+// import { ChartSourceBuildIn } from '@/plugins/charts/index';
 
 export default defineComponent({
+  name: 'ChartWrap',
   setup() {
+    // const chart = ChartSourceBuildIn['pie-charts'];
+    // const vnode = h(chart, { m: 222 });
 
   },
   render() {
-    return (<div>2</div>);
+    return (
+      <div>
+        <div>title</div>
+        {/* <div>{vnode}</div>*/}
+      </div>
+    );
   },
 });
--- src/pages/charts/index.tsx
+++ src/pages/charts/index.tsx
@@ -1,10 +1,17 @@
 import { defineComponent } from 'vue';
+import chartWrap from '@/components/chart-wrap';
 
 export default defineComponent({
+  name: 'Charts',
   setup() {
 
   },
   render() {
-    return (<div>2</div>);
+    return (
+      <div>
+        <div>22</div>
+        <div>{chartWrap}</div>
+      </div>
+    );
   },
 });
